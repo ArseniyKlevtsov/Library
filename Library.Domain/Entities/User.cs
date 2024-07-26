03361d5bb0a6191c166c1c45d4253ce91270fadd
arseniy <arseniyklevts@gmail.com>
2024-07-26 20:04:50 +0300

    add entities

--- Library.Domain/Entities/User.cs
+++ Library.Domain/Entities/User.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Library.Domain.Interfaces;
+using Microsoft.AspNetCore.Identity;
 
 namespace Library.Domain.Entities;
-public class User : IdentityUser
-{
 
+public class User : IdentityUser<Guid>, IEntity
+{
+    public ICollection<RentOrder>? RentOrders { get; set; }
 }

d78ecbbd67f4553605e2fff9c837eaabed5aa42e
arseniy <arseniyklevts@gmail.com>
2024-07-29 18:59:17 +0300

    Base repository

---
+++ Library.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace Library.Domain.Interfaces;
+
+public interface IRepository<TEntity> 
+    where TEntity : IEntity
+{
+    Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken);
+    Task<TEntity?> GetByIdAsync(Guid Id, CancellationToken cancellationToken);
+    Task AddAsync(TEntity entity, CancellationToken cancellationToken);
+    Task UpdateAsync(TEntity entity, CancellationToken cancellationToken);
+    Task DeleteAsync(TEntity entity, CancellationToken cancellationToken);
+    Task<TEntity?> GetByPredicateAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken);
+}
---
+++ Library.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,51 @@
+﻿using Library.Domain.Interfaces;
+using Library.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Library.Infrastructure.Repositories;
+
+public class BaseRepository<TEntity> : IRepository<TEntity>
+    where TEntity : class, IEntity
+{
+    protected readonly LibraryDbContext _context;
+    protected readonly DbSet<TEntity> _dbSet;
+
+    public BaseRepository(LibraryDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<TEntity>();
+    }
+
+    public async Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        return await _dbSet.AsNoTracking().ToListAsync(cancellationToken);
+    }
+
+    public async Task<TEntity?> GetByIdAsync(Guid Id, CancellationToken cancellationToken)
+    {
+        return await _dbSet.AsNoTracking().FirstAsync(entity => entity.Id == Id,cancellationToken);
+    }
+
+    public async Task AddAsync(TEntity entity, CancellationToken cancellationToken)
+    {
+        await _dbSet.AddAsync(entity, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+    public async Task UpdateAsync(TEntity entity, CancellationToken cancellationToken)
+    {
+        _context.Entry(entity).State = EntityState.Modified;
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task DeleteAsync(TEntity entity, CancellationToken cancellationToken)
+    {
+        _dbSet.Remove(entity);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task<TEntity?> GetByPredicateAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken)
+    {
+        return await _dbSet.AsNoTracking().FirstOrDefaultAsync(predicate, cancellationToken);
+    }
+}
